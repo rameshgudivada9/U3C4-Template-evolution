7c4bd49b6c2cd2ba810eb53606781174fa4337f3
rameshgudivada9 <96101948+rameshgudivada9@users.noreply.github.com>
2022-02-21 15:39:16 +0530

    fleching data

--- components/sidebar.js
+++ components/sidebar.js
@@ -1,5 +1,14 @@
 function sidebar() {
 
+    return`
+    <input type="text" id="searchbar" placeholder="       SEARCH HERE"/>
+    <div>menu</div>
+    <div>login</div>
+    <div>sign up</div>
+
+    <div>news</div>
+    <div>Api</div>`
+
 
     // return your html component here
     //Make sure to give input search box id as ""
